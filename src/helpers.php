e79e8ab22c53b6c32345d470f6c6a0c24449230d
codesinging <codesinging@gmail.com>
2020-05-06 17:09:42 +0800

    add Support/helpers

--- src/helpers.php
+++ src/helpers.php
@@ -38,3 +38,114 @@ if (!function_exists('load_config')) {
         return is_array($config) ? $config : [];
     }
 }
+
+if (!function_exists('blank')) {
+    /**
+     * Determine if the given value is "blank".
+     *
+     * @param mixed $value
+     * @return bool
+     */
+    function blank($value)
+    {
+        if (is_null($value)) {
+            return true;
+        }
+
+        if (is_string($value)) {
+            return trim($value) === '';
+        }
+
+        if (is_numeric($value) || is_bool($value)) {
+            return false;
+        }
+
+        if ($value instanceof Countable) {
+            return count($value) === 0;
+        }
+
+        return empty($value);
+    }
+}
+
+if (!function_exists('filled')) {
+    /**
+     * Determine if a value is "filled".
+     *
+     * @param mixed $value
+     * @return bool
+     */
+    function filled($value)
+    {
+        return !blank($value);
+    }
+}
+
+if (!function_exists('class_basename')) {
+    /**
+     * Get the class "basename" of the given object / class.
+     *
+     * @param string|object $class
+     * @return string
+     */
+    function class_basename($class)
+    {
+        $class = is_object($class) ? get_class($class) : $class;
+
+        return basename(str_replace('\\', '/', $class));
+    }
+}
+
+if (!function_exists('object_get')) {
+    /**
+     * Get an item from an object using "dot" notation.
+     *
+     * @param object $object
+     * @param string|null $key
+     * @param mixed $default
+     * @return mixed
+     */
+    function object_get($object, $key, $default = null)
+    {
+        if (is_null($key) || trim($key) == '') {
+            return $object;
+        }
+
+        foreach (explode('.', $key) as $segment) {
+            if (!is_object($object) || !isset($object->{$segment})) {
+                return value($default);
+            }
+
+            $object = $object->{$segment};
+        }
+
+        return $object;
+    }
+}
+
+if (!function_exists('value')) {
+    /**
+     * Return the default value of the given value.
+     *
+     * @param mixed $value
+     * @return mixed
+     */
+    function value($value)
+    {
+        return $value instanceof Closure ? $value() : $value;
+    }
+}
+
+if (!function_exists('with')) {
+    /**
+     * Return the given value, optionally passed through the given callback.
+     *
+     * @param mixed $value
+     * @param callable|null $callback
+     * @return mixed
+     */
+    function with($value, callable $callback = null)
+    {
+        return is_null($callback) ? $value : $callback($value);
+    }
+}
--- tests/HelpersTest.php
+++ tests/HelpersTest.php
@@ -7,6 +7,7 @@
 namespace CodeSinging\Support\Tests;
 
 use PHPUnit\Framework\TestCase;
+use stdClass;
 
 class HelpersTest extends TestCase
 {
@@ -14,13 +15,93 @@ class HelpersTest extends TestCase
     {
         $data = ['name' => 'helpers'];
 
-        self::assertEquals($data, load_config(__DIR__ . '/../data/config.php'));
-        self::assertEquals($data, load_config(__DIR__ . "/../data/config.json"));
-        self::assertEquals($data, load_config(__DIR__ . "/../data/config.ini"));
+        self::assertEquals($data, load_config(__DIR__ . '/data/config.php'));
+        self::assertEquals($data, load_config(__DIR__ . "/data/config.json"));
+        self::assertEquals($data, load_config(__DIR__ . "/data/config.ini"));
 
         if (function_exists('yaml_parse_file')) {
             self::assertEquals($data, load_config(__DIR__ . "/../data/config.yaml"));
         }
+    }
 
+    public function testBlank()
+    {
+        self::assertTrue(blank(null));
+        self::assertTrue(blank(''));
+        self::assertTrue(blank(' '));
+        self::assertTrue(blank([]));
+        self::assertTrue(blank(new CountableDemo()));
+
+        self::assertFalse(blank(0));
+        self::assertFalse(blank('0'));
+        self::assertFalse(blank(false));
+        self::assertFalse(blank([0]));
+        self::assertFalse(blank(new CountableDemo(1)));
+    }
+
+    public function testFilled()
+    {
+        self::assertFalse(filled(null));
+        self::assertFalse(filled(''));
+        self::assertFalse(filled(' '));
+        self::assertFalse(filled([]));
+        self::assertFalse(filled(new CountableDemo()));
+
+        self::assertTrue(filled(0));
+        self::assertTrue(filled('0'));
+        self::assertTrue(filled(false));
+        self::assertTrue(filled([0]));
+        self::assertTrue(filled(new CountableDemo(1)));
+    }
+
+    public function testClassBasename()
+    {
+        self::assertSame('Baz', class_basename('Foo\Bar\Baz'));
+        self::assertSame('Baz', class_basename('Baz'));
+    }
+
+    public function testObjectGet()
+    {
+        $class = new stdClass();
+        $class->name = new stdClass();
+        $class->name->value = 'foo';
+
+        self::assertSame('foo', object_get($class, 'name.value'));
+    }
+
+    public function testValue()
+    {
+        self::assertSame('foo', value('foo'));
+        self::assertSame('foo', value(function () {
+            return 'foo';
+        }));
+    }
+
+    public function testWith()
+    {
+        self::assertSame(10, with(10));
+        self::assertSame(10, with(5, function ($value) {
+            return $value + $value;
+        }));
+    }
+}
+
+class CountableDemo implements \Countable
+{
+    private $_count;
+
+    /**
+     * CountableDemo constructor.
+     * @param $count
+     */
+    public function __construct($count = 0)
+    {
+        $this->_count = $count;
+    }
+
+
+    public function count()
+    {
+        return $this->_count;
     }
 }
